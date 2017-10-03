9f8b34f2d17ddd432549483a02bb3b57a2ef1239
facundo <facundoiguerrero@gmail.com>
2017-10-03 15:18:50 -0300

    sos re puto

---
+++ TP1/tests/Prueba/palindrome.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+extern size_t palindrome(char* a);
+
+int main(int argc, char** argv){
+  size_t res = palindrome(argv[1]);
+
+  //printf("%s\n", res);
+  if (res) printf("palindrome!\n");
+  else printf("not palindrome!\n");
+
+  return 0;
+}
